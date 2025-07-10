0e286b5faa8a8bd80d64f9ecad60c7357b2bfe2c
Sandeep Vishwakarma <sandeepvishwakarmagkp5@example.com>
2025-07-10 13:47:11 +0530

    limit

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,13 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 50, // limit each IP to 50 requests per minute
+});
+
+app.use(limiter);
 
 export const protectRoute = async (req, res, next) => {
   try {
